cc84867b28cd6db06fe274a262f4761a28e09c87
ekeuwei <ekeuwei@gmail.com>
2023-07-01 16:29:02 +0100

    Netlify function configured

--- src/api.js
+++ src/api.js
@@ -3,6 +3,10 @@ const serverless = require('serverless-http');
 const app = require('../backend/app');
 const connectDatabase = require('../backend/config/database')
 
+if(process.env.NODE_ENV !== 'PRODUCTION') {
+    require('dotenv').config({ path: 'backend/config/config.env' });
+}
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
