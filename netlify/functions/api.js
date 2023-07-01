5aa189cdb4d242829b5447186d1359b97d3a82d5
ekeuwei <ekeuwei@gmail.com>
2023-07-01 12:44:18 +0100

    netlify function

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,14 +1,14 @@
-// const cloudinary = require('cloudinary')
+const cloudinary = require('cloudinary')
 const serverless = require('serverless-http');
 const app = require('../../backend/app');
 const connectDatabase = require('../../backend/config/database')
 
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-//     api_key: process.env.CLOUDINARY_API_KEY, 
-//     api_secret: process.env.CLOUDINARY_API_SECRET
-// })
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
-connectDatabase();
+// connectDatabase();
 
 module.exports.handler = serverless(app);
