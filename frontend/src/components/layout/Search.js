19c9be03e2c3e88229d9f14f8de9effa21d30a90
ekeuwei <ekeuwei@gmail.com>
2023-09-16 08:28:59 +0100

    Homepage adjusted

--- frontend/src/components/layout/Search.js
+++ frontend/src/components/layout/Search.js
@@ -15,16 +15,16 @@ const Search = ({ history }) => {
     }
 
     return (
-        <form onSubmit = { searchHandler } >
-            <div className="input-group bg-dark-4 rounded-pill mb-2" style={{minHeight: '50px'}}>
+        <form className='my-2' onSubmit = { searchHandler } >
+            <div className="input-group bg-dark-4 rounded-pill" style={{minHeight: '50px'}}>
                 <span className="input-group-text ms-2 border-0 bg-transparent">
                     <span className="text-white my-auto fa fa-search"></span>
                 </span>
                 <input 
                     type="text" 
-                    className="form-control ps-1 border-0 bg-transparent shadow-none text-white" 
-                    placeholder="Try “Cleaner”" 
-                    aria-label="Username"
+                    className="form-control ps-1 border-0 bg-transparent shadow-none text-dark-3" 
+                    placeholder="Find a worker" 
+                    aria-label="keyword"
                     value= {keyword}
                     aria-describedby="basic-addon1"
                     onChange = {(e) => setKeyword(e.target.value)}
