6e21eeaa17bdde94801c16dd07c8d76226a26cff
ekeuwei <ekeuwei@gmail.com>
2024-02-25 15:27:50 +0100

    Vite configuration updated to include mixed imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
     },
   },
+
 })
 
